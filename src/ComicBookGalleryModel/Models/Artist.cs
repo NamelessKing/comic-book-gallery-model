b70e2acbb2832cc5823e9220e1b7800dd8230f01
namelessking <habibur.tanjin3@gmail.com>
2019-07-20 23:05:51 +0200

    added Artist model and created many to many relationship ComicBook <----> Artist

---
+++ src/ComicBookGalleryModel/Models/Artist.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+
+namespace ComicBookGalleryModel.Models
+{
+    public class Artist
+    {
+
+        public Artist()
+        {
+            ComicBooks = new List<ComicBook>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<ComicBook>  ComicBooks { get; set; }
+
+    }
+}
--- src/ComicBookGalleryModel/Models/ComicBook.cs
+++ src/ComicBookGalleryModel/Models/ComicBook.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ComicBookGalleryModel.Models
 {
     public class ComicBook
     {
+
+        public ComicBook()
+        {
+            Artists = new List<Artist>();
+        }
+
         public int Id { get; set; }
-        public int SeriesId { get; set; }
         public int IssueNumber { get; set; }
         public string Description { get; set; }
         public DateTime PublishedOn { get; set; }
         public decimal? AverageRating { get; set; }
 
-
+        public int SeriesId { get; set; }
         public Series Series { get; set; }
+
+
+        public ICollection<Artist> Artists { get; set; }
 
 
         public string DisplayText
